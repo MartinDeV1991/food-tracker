ad711336eb000c5d9158b331490d62cfad7b413d
Martin <martin_de_v@hotmail.com>
2024-05-31 00:14:59 +0200

    retrigger upload

--- src/Pages/PhotoPage.jsx
+++ src/Pages/PhotoPage.jsx
@@ -11,6 +11,7 @@ const PhotoPage = () => {
     const handleFileChange = (event) => {
         const file = event.target.files[0];
         if (file) {
+            console.log(file)
             setPhoto(file);
         }
     };
